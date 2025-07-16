76c88b4ef3eda0f790f1842ab41758f00da04fe6
CLAUDIO ALEXANDRE DOS SANTOS - T0102815 <T0102815@santos.sp.gov.br>
2025-07-16 15:47:01 -0300

    🔧 Ajuste de porta com Render

--- src/server.js
+++ src/server.js
@@ -130,7 +130,7 @@ app.use((err, req, res, next) => {
 });
 
 // 🚀 Inicialização
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
